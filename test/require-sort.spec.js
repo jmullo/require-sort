5df4a616128cd5c3af3d3292340e16c0f06d37bf
Jussi Mullo <mulloju@gmail.com>
2016-04-10 23:42:51 +0300

    Added more tests

--- test/require-sort.spec.js
+++ test/require-sort.spec.js
@@ -7,6 +7,52 @@ var sorter = require("../src/require-sort");
 
 describe("require-sort tests", function() {
 
+    it("should handle null string", function() {
+        var sortedString = sorter.sortString(null);
+
+        assert.equal(sortedString, null);
+    });
+
+    it("should handle undefined string", function() {
+        var sortedString = sorter.sortString(undefined);
+
+        assert.equal(sortedString, undefined);
+    });
+
+    it("should handle empty string", function() {
+        var sortedString = sorter.sortString("");
+
+        assert.equal(sortedString, "");
+    });
+
+    it("should handle a one line string", function() {
+        var inputString = "alpha beta gamma delta";
+        var sortedString = sorter.sortString(inputString);
+
+        assert.equal(sortedString, inputString);
+    });
+
+    it("should handle one require statement", function() {
+        var inputString = "var foo = require(\"bar\");";
+        var sortedString = sorter.sortString(inputString);
+
+        assert.equal(sortedString, inputString);
+    });
+
+    it("should handle windows line endings", function() {
+        var inputString = "var foo = require(\"bar\");\r\n";
+        var sortedString = sorter.sortString(inputString);
+
+        assert.equal(sortedString, inputString);
+    });
+
+    it("should handle unix line endings", function() {
+        var inputString = "var foo = require(\"bar\");\n";
+        var sortedString = sorter.sortString(inputString);
+
+        assert.equal(sortedString, inputString);
+    });
+
     it("should sort basic input file", function() {
         var inputString = fs.readFileSync("./test/input/input-basic.js").toString();
         var outputString = fs.readFileSync("./test/output/output-basic.js").toString();
