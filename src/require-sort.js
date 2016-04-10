3d86189cef1464c6deaa1019355ea46e45ec54b0
Jussi Mullo <mulloju@gmail.com>
2016-04-10 23:42:42 +0300

    Added null handling

--- src/require-sort.js
+++ src/require-sort.js
@@ -7,6 +7,12 @@ var sorter = require("./sorter");
 var writer = require("./writer");
 
 function sortString(inputString) {
+    if (inputString === null) {
+        return null;
+    } else if (inputString === undefined) {
+        return undefined;
+    }
+
     var requireSections = parser.getRequireSections(inputString);
     var sortedSections = sorter.sortSections(requireSections);
     var outputString = writer.replaceSections(inputString, sortedSections);
